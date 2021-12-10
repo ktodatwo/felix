c4e523f9d03ac1dfab7f76bfb82b3db38c52859a
Katherine Keller <46583304+ktodatwo@users.noreply.github.com>
2021-12-09 22:18:11 -0600

    added ability to display new posts

--- app/routes.py
+++ app/routes.py
@@ -1,12 +1,14 @@
 from datetime import datetime
+from logging import log
 from flask import render_template, flash, redirect, url_for, request
 from flask_login import current_user, login_user, logout_user, login_required
+import flask_login
 from flask_login.utils import login_required, logout_user
 from werkzeug.urls import url_parse
 
 from app import app, db
-from app.models import User
-from app.forms import EditProfileForm, LoginForm, RegistrationForm, EmptyForm
+from app.models import Post, User
+from app.forms import EditProfileForm, LoginForm, RegistrationForm, EmptyForm, PostForm
 
 
 @app.before_request
@@ -16,22 +18,26 @@ def before_request():
         db.session.commit()
 
 
-@app.route('/')
-@app.route('/index')
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/index', methods=['GET', 'POST'])
 @login_required
 def index():
-    
-    posts = [
-        {
-            'author': {'username': 'achilles'},
-            'body': 'has anyone seen patroculis?'
-        },
-        {
-            'author': {'username': 'cerce'},
-            'body': 'just got back from the underworld, wild'
-        }
-    ]
-    return render_template('index.html', title='Home', posts=posts)
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post(body=form.post.data, author=current_user)
+        db.session.add(post)
+        db.session.commit()
+        flash('Your post is now live!')
+        return redirect(url_for('index'))
+    posts = current_user.followed_posts().all()
+    return render_template('index.html', title='Home', form=form, posts=posts)
+
+
+@app.route('/explore')
+@login_required
+def explore():
+    posts = Post.query.order_by(Post.timestamp.desc()).all()
+    return render_template('index.html', title='Explore', posts=posts)
 
 
 @app.route('/login', methods=['GET', 'POST'])
